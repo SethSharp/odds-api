dd3093103276cfc02f852748e77448ccf357a36d
Seth Sharp <sesharp@outlook.com>
2024-06-06 20:21:31 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Odds API Wrapper
+A convenient API wrapper for the Odds API, designed for Composer environments such as Laravel.
---
+++ src/OddsClient.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SethSharp\OddsApi;
+
+class OddsClient
+{
+    private $apiKey;
+    private $endpoint = 'https://api.the-odds-api.com/v4';
+}
