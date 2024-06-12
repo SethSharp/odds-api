683ad82afc63e37b969a8136e61faf9be5caa49a
Seth Sharp <sesharp@outlook.com>
2024-06-13 06:24:43 +1000

    attempt to add ext-http into actions

--- tests/Traits/UseValidatesParamsTest.php
+++ tests/Traits/UseValidatesParamsTest.php
@@ -3,12 +3,33 @@
 namespace Traits;
 
 use PHPUnit\Framework\TestCase;
+use SethSharp\OddsApi\Traits\UseValidatesParams;
 
 class UseValidatesParamsTest extends TestCase
 {
+    use UseValidatesParams;
+
     /** @test */
     public function if_param_is_not_within_required_params_throw_exception()
     {
-        $this->markTestSkipped();
+        $this->expectException(\InvalidArgumentException::class);
+
+        $this->validateParams([], [
+            'all'
+        ]);
+    }
+
+    /** @test */
+    public function will_pass_if_all_params_are_provided()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $this->validateParams([
+            'markets',
+            'regions'
+        ], [
+            'markets',
+            'regions'
+        ]);
     }
 }
