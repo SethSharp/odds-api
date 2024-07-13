3d1036580fe33d2a20318daa9c8e86afba6e5879
Seth Sharp <sesharp@outlook.com>
2024-07-13 11:16:03 +1000

    add afl sport enum

--- src/Enums/SportsEnum.php
+++ src/Enums/SportsEnum.php
@@ -6,4 +6,5 @@ enum SportsEnum: string
 {
     // todo: fill in with the rest of the sports from  https://the-odds-api.com/sports-odds-data/sports-apis.html
     case RUGBYLEAGUE_NRL = 'rugbyleague_nrl';
+    case AUSSIERULES_AFL = 'aussierules_afl';
 }
